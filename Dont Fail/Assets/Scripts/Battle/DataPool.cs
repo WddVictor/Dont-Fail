88cb3034ce80496f22a20fb50ae128e1fa66f7f7
WddVictor <908704496@qq.com>
2018-12-25 23:11:44 +0800

    Complete Drinks and some buffs and only use unitypackage

--- Dont Fail/Assets/Scripts/Battle/DataPool.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class DataPool{
-    public static int playerGetDamageTimes = 0;
-    public static int monsterGetDamageTimes = 0;
-
-}
